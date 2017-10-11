6a17099c9ab17eff869ac836e2d20439099b3153
Matthew Renn <rennmatthewp@gmail.com>
2017-10-11 14:49:22 -0600

    initial game file tests

---
+++ test/game-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const assert = chai.assert;
+const game = require('../lib/game.js');
+
+describe('Game', () => {
+  let game;
+
+  beforeEach( () => {
+    game = new Game();
+  })
+
+  it('should have a game array', () => {
+    assert.equal(game.gameArray, )
+  })
+
+
+                   // const lastPlayer = this.gameArray[5][0].player
+                   // assert.equal(lastPlayer, 1)
+})
